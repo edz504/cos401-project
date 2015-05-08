da91d68ee734f0be3e4778207b71834aaa3d9786
edz504 <eddiedzhou@gmail.com>
2015-05-07 21:48:56 -0400

    code to take google form results into pd

--- labeling/getmnlist.py
+++ labeling/getmnlist.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 with open('googleform.csv', 'rb') as f:
     lines = f.read().split('\n')
 
@@ -7,3 +9,14 @@ mns = [l[l.find('[') + 1:l.find(']')] for l in mn_list if '[' in l]
 with open('mn_list_from.txt', 'wb') as f:
     for mn in mns:
         f.write('%s\n' % mn)
+
+names = [l.split(',')[len(l.split(',')) - 1] + ' Borda' for l in lines[1:]]
+vote_df = pd.DataFrame(index=mns, columns=names)
+N = 10
+
+i = 0
+for vote in lines[1:]:
+    vote_df[vote_df.columns[i]] = [(N - int(v)) for v in vote.split(',')[1:171]]
+    i += 1
+
+y = list(vote_df.sum(axis=1))
